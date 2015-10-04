8caefbeb5baeb4530f73e6dab63decc90b48d44a
Stefan Stegerer <stefan-stegerer@gmx.de>
2015-10-04 14:46:53 +0200

    all-start-is-hard
    Created the Android-Studio Projekt and named it.
    No Code atm

---
+++ vjos-mobile-0.1/app/src/main/java/com/example/zeppelin/vjos_mobile_01/VjosMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zeppelin.vjos_mobile_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VjosMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vjos_main);
+    }
+}
